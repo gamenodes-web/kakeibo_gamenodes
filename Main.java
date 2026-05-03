a8766d7eaced6fe48073441b1039004103eb6946
gamenodes <curry.and.takoyaki.01@gmail.com>
2026-05-03 21:05:01 +0900

    繰り返し処理を追加

--- Main.java
+++ Main.java
@@ -1,12 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        int sunyuu = 243000;
-        int shishutsu = 300000;
-
-        if (shishutsu > sunyuu) {
-            System.out.println("赤字です");
-        } else {
-            System.out.println("黒字です");
+        for (int i = 1; i <= 12; i++) {
+            System.out.println(i + "月");
         }
     }
 }
