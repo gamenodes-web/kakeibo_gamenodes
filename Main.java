f942af81e7d4fa1249626bf79b323f2ad0f36e6b
gamenodes <curry.and.takoyaki.01@gmail.com>
2026-05-03 21:14:43 +0900

    メソッド処理を追加

--- Main.java
+++ Main.java
@@ -1,7 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i = 1; i <= 12; i++) {
-            System.out.println(i + "月");
-        }
+        keisan(243000, 41000);
+    }
+
+    static void keisan(int sunyuu, int shishutsu) {
+        int chokin = sunyuu - shishutsu;
+        System.out.println("収入：" + sunyuu);
+        System.out.println("支出：" + shishutsu);
+        System.out.println("貯金：" + chokin);
     }
 }
