e33765ff866558770da0cf911052f30e4c4f3efe
gamenodes <curry.and.takoyaki.01@gmail.com>
2026-05-03 18:57:40 +0900

    条件分岐を追加~

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int sunyuu = 243000;
+        int shishutsu = 300000;
+
+        if (shishutsu > sunyuu) {
+            System.out.println("赤字です");
+        } else {
+            System.out.println("黒字です");
+        }
+    }
+}
